28a28aafd2c58d562f874a06e2eae60c71152e37
nickveil <31191433+nickveil@users.noreply.github.com>
2017-10-22 12:07:33 -0400

    Console log clean up and changed button message

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
     <div id="simon">
 
-      <div id="status" v-bind:src="displayMessage" >
+      <div id="status">
         {{displayMessage}}
       </div>
 
@@ -42,7 +42,7 @@
     </div>
 
     <div id="controls" class="row">
-      <div class="col"><button class="start" v-on:click="start">Start</button></div>
+      <div class="col"><button class="start" v-on:click="start" >{{ buttonMessage }}</button></div>
     </div>
 
     <div id="history">
@@ -65,7 +65,8 @@ export default {
       isActive:false,
       currentLight:"",
       iterationCounter:0,
-      displayMessage:''
+      displayMessage:'',
+      buttonMessage:'Start'
     }
   },
   computed: {
@@ -78,7 +79,7 @@ export default {
   methods: {
     
     start: function() {
-      this.sequence = []; // will need to make active and reset sequence in the return to blank
+      this.sequence = [];
       this.addToSequence();
       this.playSequence(this.sequence);
       this.startTimer();
@@ -86,98 +87,69 @@ export default {
       this.displayMessage='';
 
     },
+
     chooseRandomLight: function() {
       var index = Math.floor(Math.random() * 4);
-      //console.log(index);
       return this.lights[index];
     },
-    addToSequence: function() {
-      
-      this.sequence.push(this.chooseRandomLight(),'blank');
 
-      // this.sequence.push("blank");
+    addToSequence: function() { 
+      this.sequence.push(this.chooseRandomLight(),'blank');
+      this.buttonMessage="Restart";
+
     },
+
     playSequence: function(lightSelected) {
       // might be cool to increase speed after certain levels are achieved
       // Such as decrease time by 250ms after 10 sequences
       var self=this;
-      //console.log(this.sequence);
-      
+
       lightSelected.forEach(function(el,index){
-        setTimeout(function(){
-          self.changeLightState(index);
-
-          //console.log("inside timeOut: ",el);
-
-        }, index*400);
+        setTimeout(function(){self.changeLightState(index);}, index*400);
         self.isActive=false;
-
       });
-      // console.log("tracer forEach: ",el);
-
     },
 
     changeLightState: function(iteration){
-      var self=this;
-      console.log('iteration: ',iteration);
-      console.log("currentColor: ",this.sequence[iteration]);
+
       this.isActive=true;
       this.currentLight=self.sequence[iteration]; 
-      // self.isActive=false;
+     
     },
+
     captureTap: function(color) {
 
       this.taps=color;
       this.currentLight=color;
-      
 
-      console.log("Sequence_color: ",this.sequence[this.iterationCounter]," Tapped_color: ",this.taps);
-      console.log("iterationCounter",this.iterationCounter);
-
-      if(this.sequence[this.iterationCounter]=== this.taps){
+      if(this.sequence[this.iterationCounter] === this.taps){
         this.iterationCounter+=2;
-        console.log("correct Answer");
-        console.log("this.iterationCounter: ",this.iterationCounter);
-        console.log("this.sequence[this.sequence.length-1]: ",this.sequence.length);
 
         if (this.iterationCounter===this.sequence.length){
-          console.log("hello");
           this.currentLight=false;
           this.addToSequence();
           this.playSequence(this.sequence);
           this.startTimer();
           this.iterationCounter=0;
-        }else{
-          
-
-
         }
 
-
-
-       
       }else{
         
         if(this.sequence.length/2 > this.longest){
           this.currentLight=false;
           this.longest=this.sequence.length/2;
-          this.displayMessage="Congrats!!! This is your longest sequence yet!";
-        }else{
-          this.currentLight=false;
-          this.displayMessage="So close, try again.";
+          this.displayMessage="Sorry, not quite right. But hey, this is your longest sequence yet!";
+          this.buttonMessage="Try Again";
         }
-
+        else{
+          this.currentLight=false;
+          this.displayMessage="Sorry, not quite right. But you're close, give it another try.";
+          this.buttonMessage="Try Again";
+        }
       };
 
       console.log(this.taps);
-      // log key strokes
-
-      // bring in sequence 
-      // compare key strokes to next sequence element
-
-
     },
-
 
     startTimer: function() {
     }
@@ -186,75 +158,75 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-#simon {
-  padding: 20px;
-}
-#controls {
-  padding-bottom: 20px;
-}
-#status {
-  padding-bottom: 20px;
-}
-.row {
-}
-.col {
-  display: inline-block;
-}
-.start {
-  padding: 10px;
-  font-size: 18px;
-}
-.light {
-  margin: 20px;
-  border: 1px solid #000;
-}
-#red {
-  height: 100px;
-  width: 100px;
-  background: #E53A40;
-  opacity: 0.1;
-}
-#yellow {
-  height: 100px;
-  width: 100px;
-  background: #EFDC05;
-  opacity: 0.1;
-}
-#green {
-  height: 100px;
-  width: 100px;
-  background: #75D701;
-  opacity: 0.1;
-}
-#blue {
-  height: 100px;
-  width: 100px;
-  background: #30A9DE;
-  opacity: 0.1;
-}
-.bright {
-  opacity: 1.0 !important;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  #simon {
+    padding: 20px;
+  }
+  #controls {
+    padding-bottom: 20px;
+  }
+  #status {
+    padding-bottom: 20px;
+  }
+  .row {
+  }
+  .col {
+    display: inline-block;
+  }
+  .start {
+    padding: 10px;
+    font-size: 18px;
+  }
+  .light {
+    margin: 20px;
+    border: 1px solid #000;
+  }
+  #red {
+    height: 100px;
+    width: 100px;
+    background: #E53A40;
+    opacity: 0.1;
+  }
+  #yellow {
+    height: 100px;
+    width: 100px;
+    background: #EFDC05;
+    opacity: 0.1;
+  }
+  #green {
+    height: 100px;
+    width: 100px;
+    background: #75D701;
+    opacity: 0.1;
+  }
+  #blue {
+    height: 100px;
+    width: 100px;
+    background: #30A9DE;
+    opacity: 0.1;
+  }
+  .bright {
+    opacity: 1.0 !important;
+  }
 </style>
